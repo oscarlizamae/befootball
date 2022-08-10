3f55b7c8f0737ce50a236132d648f12625004368
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-10 15:59:09 -0600

    Create a new object in Screen sealed class

--- app/src/main/java/com/gooner/befootball/Screen.kt
+++ app/src/main/java/com/gooner/befootball/Screen.kt
@@ -2,6 +2,7 @@ package com.gooner.befootball
 
 sealed class Screen(val route: String) {
     object SplashScreen: Screen("splash_screen")
+    object HomeScreen: Screen("home_screen")
 
     fun withArgs(vararg args: String): String {
         return buildString {
