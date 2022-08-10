db8f5eb0da5b84d5f25ea374a40e1f35eae42eec
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-10 15:59:39 -0600

    Add background color for both light and dark modes

--- app/src/main/java/com/gooner/befootball/ui/theme/Color.kt
+++ app/src/main/java/com/gooner/befootball/ui/theme/Color.kt
@@ -9,6 +9,7 @@ val StatusBarColorLight = Color(0xFFFFFFFF)
 val NavigationBarColorSplashScreenLight = Color(0xFFFFFFFF)
 val NavigationBarColorLight = Color(0xFFEAEAEA)
 val LogoColorLight = Color(0xFF000000)
+val BackgroundColorLight = Color(0xFFFFFFFF)
 
 // Colors DarkTheme
 val ColorPrimaryDark = Color(0xFF373738)
@@ -17,3 +18,4 @@ val StatusBarColorDark = Color(0xFF373738)
 val StatusBarColorSplashScreenDark = Color(0xFF181B1E)
 val NavigationBarColorDark = Color(0xFF181B1E)
 val LogoColorDark = Color(0xFFFFFFFF)
+val BackgroundColorDark = Color(0xFF2A2B2D)
