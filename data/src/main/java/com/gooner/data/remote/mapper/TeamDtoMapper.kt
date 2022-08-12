d032709e743960dd7b3f5141da49814c4dfedd31
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:23:25 -0600

    Add a new mapper to map the information of the teams retrived from the api

---
+++ data/src/main/java/com/gooner/data/remote/mapper/TeamDtoMapper.kt
@@ -0,0 +1,24 @@
+package com.gooner.data.remote.mapper
+
+import com.gooner.data.remote.dto.TeamDto
+import com.gooner.data.util.EntityMapper
+import com.gooner.domain.model.Team
+
+class TeamDtoMapper : EntityMapper<TeamDto, Team> {
+    override fun mapFromEntity(entity: TeamDto): Team =
+        Team(
+            id = entity.id,
+            name = entity.name,
+            logoUrl = entity.logoUrl
+        )
+
+    override fun mapToEntity(model: Team): TeamDto =
+        TeamDto(
+            id = model.id,
+            name = model.name,
+            logoUrl = model.logoUrl
+        )
+
+    override fun mapFromEntityList(entities: List<TeamDto>): List<Team> =
+        entities.map { mapFromEntity(it) }
+}
