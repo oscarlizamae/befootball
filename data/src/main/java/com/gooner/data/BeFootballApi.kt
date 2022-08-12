071cc075b49a816794f9ce3789f2427ba8a68b23
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:20:45 -0600

    Add a new endpoint to retrieve the information of the live matches

--- data/src/main/java/com/gooner/data/BeFootballApi.kt
+++ data/src/main/java/com/gooner/data/BeFootballApi.kt
@@ -1,16 +1,23 @@
 package com.gooner.data
 
+import com.gooner.data.remote.dto.FixtureResponse
 import com.gooner.data.remote.dto.LeagueResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface BeFootballApi {
-    
+
+    // Leagues
     @GET("leagues")
     suspend fun getCurrentLeagues(
         @Query("current") current: Boolean = true,
         @Query("season") season: String = ""
     ) : LeagueResponse
+
+    @GET("fixtures")
+    suspend fun getLiveFixtures(
+        @Query("live") live: String = "all"
+    ) : FixtureResponse
 
 }
