cff544a39f2320b380e58662985cdc6cac4cbf73
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 11:56:28 -0600

    Create a singleton injecion of an OkHttpBuilder instance

--- data/src/main/java/com/gooner/data/di/NetworkModule.kt
+++ data/src/main/java/com/gooner/data/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package com.gooner.data.di
 
 import com.gooner.data.BeFootballApi
+import com.gooner.data.util.createHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -8,12 +9,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 private const val BASE_URL = "https://api-football-v1.p.rapidapi.com/v3/"
 
 val networkModule = module {
+    single { createHttpClient() }
     single { GsonConverterFactory.create() }
     single {
         Retrofit
             .Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(get<GsonConverterFactory>())
+            .client(get())
             .build()
     }
     single {
