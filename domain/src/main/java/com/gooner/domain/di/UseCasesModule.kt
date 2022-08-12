56d372fe2b30685caa10e978f131aceadb8065da
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:20:02 -0600

    Provide the use case that retrieves the live matches

--- domain/src/main/java/com/gooner/domain/di/UseCasesModule.kt
+++ domain/src/main/java/com/gooner/domain/di/UseCasesModule.kt
@@ -1,9 +1,9 @@
 package com.gooner.domain.di
 
-import com.gooner.domain.usecases.GetCurrentLeagues
-import com.gooner.domain.usecases.GetCurrentLeaguesImpl
+import com.gooner.domain.usecases.*
 import org.koin.dsl.module
 
 val useCasesModule = module {
     factory<GetCurrentLeagues> { GetCurrentLeaguesImpl(get()) }
+    factory<GetLiveMatches> { GetLiveMatchesImpl(get()) }
 }
