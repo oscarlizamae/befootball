2d04f3e173724d65bbf02af0714e3cba92ffd6c4
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:18:54 -0600

    Create models to show the information of one league

--- domain/src/main/java/com/gooner/domain/model/League.kt
+++ domain/src/main/java/com/gooner/domain/model/League.kt
@@ -3,6 +3,6 @@ package com.gooner.domain.model
 data class League(
     val id: Int,
     val name: String,
-    val type: String,
-    val logoUrl: String
+    val type: String? = "",
+    val logoUrl: String? = ""
 )
