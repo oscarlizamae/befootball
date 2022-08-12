0a4d75506653bce5e23d79a7a53332b2314dcd84
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:26:56 -0600

    Fetch the information of the live matche

--- app/src/main/java/com/gooner/befootball/featurehome/HomeScreenViewModel.kt
+++ app/src/main/java/com/gooner/befootball/featurehome/HomeScreenViewModel.kt
@@ -1,20 +1,25 @@
 package com.gooner.befootball.featurehome
 
+import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.gooner.domain.model.Fixture
 import com.gooner.domain.model.League
 import com.gooner.domain.usecases.GetCurrentLeagues
+import com.gooner.domain.usecases.GetLiveMatches
 import com.gooner.domain.util.ResponseResult
 import kotlinx.coroutines.launch
 
 class HomeScreenViewModel(
-    private val getCurrentLeagues: GetCurrentLeagues
+    private val getCurrentLeagues: GetCurrentLeagues,
+    private val getLiveMatches: GetLiveMatches
 ) : ViewModel() {
 
-    val leagues = mutableStateOf<List<League>>(emptyList())
+    @SuppressLint("MutableCollectionMutableState")
+    val leagues = mutableStateOf<List<League>>(mutableListOf())
+    val liveMatches = mutableStateOf<List<Fixture>>(emptyList())
 
     fun fetchCurrentLeagues() {
         viewModelScope.launch {
@@ -27,6 +32,30 @@ class HomeScreenViewModel(
                     Log.e("HomeScreen", result.toString())
                 }
             }
+        }
+    }
+
+    fun fetchLivesMatches() {
+        viewModelScope.launch {
+            when (val result = getLiveMatches()) {
+                is ResponseResult.Success -> {
+                    liveMatches.value = result.data
+                    filterLeagues(liveMatches.value)
+                }
+                else -> {
+                    Log.e("HomeScreen", result.toString())
+                }
+            }
+        }
+    }
+
+    private fun filterLeagues(liveMatches: List<Fixture>) {
+        val uniqueLeagues: MutableList<League> = mutableListOf()
+        for (fixture in liveMatches) {
+            uniqueLeagues.add(fixture.league)
+        }
+        uniqueLeagues.let {
+            leagues.value = it.distinct().toList()
         }
     }
 
