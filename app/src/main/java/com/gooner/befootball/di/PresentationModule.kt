5ed29b45a4ddd69b70e2ff1ea43ce0e4221a2392
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:27:42 -0600

    Inject dependencies to the 'HomeScreenViewModel' class

--- app/src/main/java/com/gooner/befootball/di/PresentationModule.kt
+++ app/src/main/java/com/gooner/befootball/di/PresentationModule.kt
@@ -5,5 +5,5 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
-    viewModel { HomeScreenViewModel(get()) }
+    viewModel { HomeScreenViewModel(get(), get()) }
 }
