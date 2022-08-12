9c1869e3e64fc0bd69d77e0d6a0ac0aca4b6d86a
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:21:39 -0600

    Add the models that holds the information of a fixture

---
+++ data/src/main/java/com/gooner/data/remote/dto/FixtureDto.kt
@@ -0,0 +1,35 @@
+package com.gooner.data.remote.dto
+
+import com.gooner.domain.model.League
+
+data class FixtureResponse(
+    val response: List<FixtureResponseInfo>
+)
+
+data class FixtureResponseInfo(
+    val fixture: FixtureDto,
+    val league: League,
+    val teams: FixtureTeams,
+    val goals: FixtureGoals
+)
+
+data class FixtureDto(
+    val id: Int,
+    val status: FixtureStatus
+)
+
+data class FixtureTeams(
+    val home: TeamDto,
+    val away: TeamDto
+)
+
+data class FixtureStatus(
+    val long: String,
+    val short: String,
+    val elapsed: Int
+)
+
+data class FixtureGoals(
+    val home: Int,
+    val away: Int
+)
