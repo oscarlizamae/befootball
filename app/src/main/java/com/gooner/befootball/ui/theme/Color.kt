972c5db2efa99a4cdd7a76d939a1d49b673bcf3f
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-12 16:33:38 -0600

    Add a new color to Colors file

--- app/src/main/java/com/gooner/befootball/ui/theme/Color.kt
+++ app/src/main/java/com/gooner/befootball/ui/theme/Color.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.graphics.Color
 val LiveMatchesLeagueColor = Color(0xFF262626)
 val LiveMatchCardColor = Color(0xFF2648D1)
 val IndicatorCircleGreen = Color(0xFF33FF00)
+val IndicatorCircleYellow = Color(0xFFFBC02D)
 
 // Colors LightTheme
 val ColorPrimaryLight = Color(0xFFFFFFFF)
