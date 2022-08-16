28fc84baf215791757461f1f78e834603807a37f
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-16 15:05:35 -0600

    Add a new file that contains all the top bars used in the app

---
+++ app/src/main/java/com/gooner/befootball/util/TopAppBar.kt
@@ -0,0 +1,61 @@
+package com.gooner.befootball.util
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.gooner.befootball.R
+
+@Composable
+fun BaseTopAppBar(
+    content: @Composable () -> Unit
+) {
+    TopAppBar(
+        backgroundColor = Color.White,
+        elevation = 1.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 8.dp, end = 8.dp)
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun RegularTopAppBar(
+    title: String,
+    onBackIconClick: () -> Unit
+) {
+    BaseTopAppBar {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = { onBackIconClick() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back_arrow),
+                    contentDescription = null
+                )
+            }
+            Text(
+                modifier = Modifier.padding(start = 24.dp),
+                text = title,
+                style = typography.h6
+            )
+        }
+    }
+}
