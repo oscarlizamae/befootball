12d1e46f18377064747246172e494357f2500f2d
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-16 15:07:22 -0600

    Create a new route to the Live Matches screen

--- app/src/main/java/com/gooner/befootball/Screen.kt
+++ app/src/main/java/com/gooner/befootball/Screen.kt
@@ -3,6 +3,7 @@ package com.gooner.befootball
 sealed class Screen(val route: String) {
     object SplashScreen: Screen("splash_screen")
     object HomeScreen: Screen("home_screen")
+    object LiveMatches: Screen("live_matches")
 
     fun withArgs(vararg args: String): String {
         return buildString {
