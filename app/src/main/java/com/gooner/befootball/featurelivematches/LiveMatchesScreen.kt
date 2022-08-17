64a4642db159c27edccdf79abc905c6c05d02a4b
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-17 17:33:35 -0600

    Remove unsued imports from LiveMatchesScreen file

--- app/src/main/java/com/gooner/befootball/featurelivematches/LiveMatchesScreen.kt
+++ app/src/main/java/com/gooner/befootball/featurelivematches/LiveMatchesScreen.kt
@@ -1,7 +1,5 @@
 package com.gooner.befootball.featurelivematches
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -18,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
