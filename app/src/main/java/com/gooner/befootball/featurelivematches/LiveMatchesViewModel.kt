a4f0ccd6eefe633abca6b7b102f7f674ee4ce534
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-17 17:30:40 -0600

    Create a view model for the live matches screen

---
+++ app/src/main/java/com/gooner/befootball/featurelivematches/LiveMatchesViewModel.kt
@@ -0,0 +1,31 @@
+package com.gooner.befootball.featurelivematches
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gooner.domain.model.Fixture
+import com.gooner.domain.usecases.GetLiveMatches
+import com.gooner.domain.util.ResponseResult
+import kotlinx.coroutines.launch
+
+class LiveMatchesViewModel(
+    private val getLiveMatches: GetLiveMatches
+) : ViewModel() {
+
+    val liveMatches = mutableStateOf<List<Fixture>>(emptyList())
+
+    fun fetchLiveMatches() {
+        viewModelScope.launch {
+            when (val result = getLiveMatches()) {
+                is ResponseResult.Success -> {
+                    liveMatches.value = result.data
+                }
+                else -> {
+                    Log.e("Error", result.toString())
+                }
+            }
+        }
+    }
+
+}
