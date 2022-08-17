c0f573c04fc0b34812956742375143a431d448b8
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-17 17:34:34 -0600

    Remove unused function from HomeScreenViewModel class

--- app/src/main/java/com/gooner/befootball/featurehome/HomeScreenViewModel.kt
+++ app/src/main/java/com/gooner/befootball/featurehome/HomeScreenViewModel.kt
@@ -7,33 +7,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gooner.domain.model.Fixture
 import com.gooner.domain.model.League
-import com.gooner.domain.usecases.GetCurrentLeagues
 import com.gooner.domain.usecases.GetLiveMatches
 import com.gooner.domain.util.ResponseResult
 import kotlinx.coroutines.launch
 
 class HomeScreenViewModel(
-    private val getCurrentLeagues: GetCurrentLeagues,
     private val getLiveMatches: GetLiveMatches
 ) : ViewModel() {
 
     @SuppressLint("MutableCollectionMutableState")
     val leagues = mutableStateOf<List<League>>(mutableListOf())
     val liveMatches = mutableStateOf<List<Fixture>>(emptyList())
-
-    fun fetchCurrentLeagues() {
-        viewModelScope.launch {
-            when(val result = getCurrentLeagues()) {
-                is ResponseResult.Success -> {
-                    leagues.value = result.data
-                    Log.d("LeaguesResult", leagues.value.toString())
-                }
-                else -> {
-                    Log.e("HomeScreen", result.toString())
-                }
-            }
-        }
-    }
 
     fun fetchLivesMatches() {
         viewModelScope.launch {
