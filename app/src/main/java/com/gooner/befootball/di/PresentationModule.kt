26cb8d66659715c03a86a549e5ae2997800eb9d3
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-17 17:32:24 -0600

    Provide the view model to the Live Matches screen through Koin

--- app/src/main/java/com/gooner/befootball/di/PresentationModule.kt
+++ app/src/main/java/com/gooner/befootball/di/PresentationModule.kt
@@ -1,9 +1,11 @@
 package com.gooner.befootball.di
 
 import com.gooner.befootball.featurehome.HomeScreenViewModel
+import com.gooner.befootball.featurelivematches.LiveMatchesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
     viewModel { HomeScreenViewModel(get(), get()) }
+    viewModel { LiveMatchesViewModel(get()) }
 }
