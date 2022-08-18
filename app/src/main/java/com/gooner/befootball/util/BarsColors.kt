bfa6d882dc18cbb64eb66bac3dc338c4600fb365
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-18 14:42:36 -0600

    Set default color to navigation bar in the BarsColors composable function

--- app/src/main/java/com/gooner/befootball/util/BarsColors.kt
+++ app/src/main/java/com/gooner/befootball/util/BarsColors.kt
@@ -3,11 +3,12 @@ package com.gooner.befootball.util
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.gooner.befootball.ui.theme.NavigationBarColorLight
 
 @Composable
 fun BarsColors(
     statusBarColor: Color,
-    navigationBarColor: Color
+    navigationBarColor: Color = NavigationBarColorLight
 ) {
     rememberSystemUiController().apply {
         this.setStatusBarColor(statusBarColor)
