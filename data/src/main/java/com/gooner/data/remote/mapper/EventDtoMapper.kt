d9efe799fe2255b6021cb1743b8bbc274985a118
Oscar Lizama <oscar_lizama@unicomer.com>
2022-08-18 16:14:40 -0600

    Create a new mapper for Event entity

---
+++ data/src/main/java/com/gooner/data/remote/mapper/EventDtoMapper.kt
@@ -0,0 +1,30 @@
+package com.gooner.data.remote.mapper
+
+import com.gooner.data.remote.dto.EventDto
+import com.gooner.data.util.EntityMapper
+import com.gooner.domain.model.Event
+import com.gooner.domain.model.EventTime
+
+class EventDtoMapper(
+    private val teamDtoMapper: TeamDtoMapper,
+    private val playerDtoMapper: PlayerDtoMapper
+) : EntityMapper<EventDto, Event> {
+    override fun mapFromEntity(entity: EventDto): Event =
+        Event(
+            type = entity.type,
+            detail = entity.detail,
+            time = EventTime(
+                elapsed = entity.time.elapsed,
+                extra = entity.time.extra
+            ),
+            player = playerDtoMapper.mapFromEntity(entity.player),
+            team = teamDtoMapper.mapFromEntity(entity.team)
+        )
+
+    /* override fun mapToEntity(model: Event): EventDto {
+        TODO("Not yet implemented")
+    } */
+
+    override fun mapFromEntityList(entities: List<EventDto>): List<Event> =
+        entities.map { mapFromEntity(it) }
+}
