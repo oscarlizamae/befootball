b8b7e52a60ee24a073c40de862cc2a100b61ee46
Oscar Lizama <oscar_lizama@unicomer.com>
2022-10-20 14:44:02 -0600

    Add a new data class called Assit in the Event file

--- domain/src/main/java/com/gooner/domain/model/Event.kt
+++ domain/src/main/java/com/gooner/domain/model/Event.kt
@@ -4,6 +4,7 @@ data class Event(
     val type: String? = "",
     val detail: String? = "",
     val player: Player = Player(),
+    val assist: Assist = Assist(),
     val team: Team,
     val time: EventTime
 )
@@ -11,4 +12,9 @@ data class Event(
 data class EventTime(
     val elapsed: Int? = 0,
     val extra: Int? = 0
+)
+
+data class Assist(
+    val id: Int? = null,
+    val name: String? = null
 )
