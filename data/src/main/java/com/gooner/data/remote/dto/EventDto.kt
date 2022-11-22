8243f7e703cd50cf1f698b0f5a2d1ea90d446666
Oscar Lizama <oscar_lizama@unicomer.com>
2022-11-21 18:42:00 -0600

    Modify the 'EventDto' data class

--- data/src/main/java/com/gooner/data/remote/dto/EventDto.kt
+++ data/src/main/java/com/gooner/data/remote/dto/EventDto.kt
@@ -1,9 +1,12 @@
 package com.gooner.data.remote.dto
 
+import com.gooner.domain.model.Assist
+
 data class EventDto(
     val type: String? = "",
     val detail: String? = "",
     val player: PlayerDto = PlayerDto(),
+    val assist: Assist = Assist(),
     val team: TeamDto,
     val time: EventTimeDto
 )
@@ -11,4 +14,9 @@ data class EventDto(
 data class EventTimeDto(
     val elapsed: Int? = 0,
     val extra: Int? = 0
+)
+
+data class Assist(
+    val id: Int? = 0,
+    val name: String? = ""
 )
